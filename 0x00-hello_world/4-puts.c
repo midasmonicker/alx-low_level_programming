1b21fa5f5d3a74e713c5e44394308cfee542cc4d
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-06-30 16:29:52 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 
 /**
 
- * main - Entry point
+* main -entry point
 
- *
+* Return: Always 0 (sucess)
 
- * Return: Always 0 (Success)
-
- */
+*/
 
 int main(void)
 
