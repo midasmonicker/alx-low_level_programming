230981e08eab918721f4a98a43a222f78f51c47a
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-06-30 16:09:01 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 
 /**
-
- * main - Entry point
-
  *
-
- * Return: Always 0 (Success)
-
- */
+ * * main -entry point
+ *
+ * * Return: Always 0 (sucess)
+ *
+ * */
 
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 
 }
