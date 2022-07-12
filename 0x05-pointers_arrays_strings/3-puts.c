cdbd5506ea56732b97fc313a13c3edf044d7a6ee
midasmonicker <michaelonyeweke@yahoo.com>
2022-07-12 09:53:35 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - to write a string to the output stream
+ * @str: string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	char *str;
+
+	str = " ";
+	_puts(str);
+	return (0);
+}
