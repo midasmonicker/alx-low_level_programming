8a90ea957be7669f2f0956606c69a263284e4fbb
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-08-04 00:43:09 +0100

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,41 @@
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * main - Prints result of an operation
- * @argc: Number of arguments
- * @argv: Value of arguments
- * Return: Returns an int.
-**/
+ * main - when user runs main,
+ * user should give two integers and an operator and
+ * main will calculate the math via a function pointer.
+ * prints sum, difference, product, dividend, or remainder
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on sucess
+ */
 
 int main(int argc, char *argv[])
 {
-
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-
-if (
-(strcmp(argv[2], "+") &&
-strcmp(argv[2], "-") &&
-strcmp(argv[2], "*") &&
-strcmp(argv[2], "/") &&
-strcmp(argv[2], "%"))
-)
-{
-printf("Error\n");
-exit(99);
-}
-
-if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-&& atoi(argv[3]) == 0)
-{
-printf("Error\n");
-exit(100);
-}
-
-printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-
-return (0);
+	int n1, n2;
+	int (*f)(int, int);
+	/* validate input */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/* convert user input to ints and point to correct operator function */
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL || (argv[2][1] != '\0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", f(n1, n2)); /* calculate via function ptr */
+	return (0);
 }
