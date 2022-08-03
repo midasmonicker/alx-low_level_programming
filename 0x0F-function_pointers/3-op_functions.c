8e15601702304c72ce816eaf1b2fdd750a2818ef
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-08-03 19:50:36 +0100

    calculations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+/**
+ * op_add - add 2 numbers
+ * @a: first int
+ * @b: second int
+ * return: result
+ */
+int op_add(int a, int b)
+{
+	return (a+b);
+}
+
+/**
+ * op_sub - subtract 2 numbers
+ * @a: first int
+ * @b: second int
+ * return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies 2 numbers
+ * @a: first int
+ * @b: second int
+ * return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide 2 numbers
+ * @a: first int
+ * @b: second int
+ * return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus 2 numbers
+ * @a: first int
+ * @b: second int
+ * return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
