f031d65508fc6457d3ebe2693ac6688ce6d560c3
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-08-05 00:24:58 +0100

    print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,18 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct printer - defines a printer
+ * @symbol: a data type
+ * @print: prints a data type
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 #endif
