70f7f79972937a56abe7ef7efd1fcb032ff3a7ff
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-10-09 18:22:14 +0100

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 
 /**
+ * print_dlistint - prints all elements of the linked list
+ * @h: head node of the doubly linked list
+ *
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
